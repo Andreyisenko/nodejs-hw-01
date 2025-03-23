d4fcd7a64e7df6fe738f12665cf2d797d3b1d2ed
Andrey Isenko <aisenko87@gmail.com>
2025-03-23 18:20:48 +0200

    add hw-1

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,10 @@
-export const addOneContact = async () => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+export const addOneContact = async () => {
+  const contList = await readContacts();
+  const newList = createFakeContact();
+  writeContacts([...contList, newList]);
+};
 
 addOneContact();
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,11 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+const generateContacts = async (number) => {
+  const contList = await readContacts();
+
+  const newList = Array(number).fill(0).map(createFakeContact);
+  await writeContacts([...contList, ...newList]);
+};
 
 generateContacts(5);
